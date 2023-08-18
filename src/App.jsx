9bf6bc521182a8ee0fe3c467375974b595b8db26
pujamahankuda <100012306+pujamahankuda@users.noreply.github.com>
2023-08-18 23:32:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import KanbanBoard from "./components/KanbanBoard";
+
+
+function App() {
+  return (
+    <div className="App">
+      <KanbanBoard />
+    </div>
+  );
+}
+
+export default App;
